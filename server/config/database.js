e6a641c350a03b810015017f37414c8f82e65c0b
lalitisbhatia <rmlbpoc@yahoo.com>
2015-04-09 20:37:06 -0400

    updated db config to point to local test db when using test env

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,8 @@
 var env = process.env.NODE_ENV;
 console.log('the node env is ' + env);
-var db = env=='test'?'wwtestdb':'wwpilotdb';
+var urlLocal = "mongodb://localhost:27017/test";
+var urlRemote = "mongodb://rmlbmongo:titanic0215@oceanic.mongohq.com:10006/wwpilotdb"
+var url = env=='test'?urlLocal:urlRemote;
 module.exports = {
-    url:'mongodb://rmlbmongo:titanic0215@oceanic.mongohq.com:10006/' + db
+    url: url
 };
