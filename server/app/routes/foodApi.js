64a7fa8a0f89143dd96d28f8d4bb93c7a1211e73
lalitisbhatia <rmlbpoc@yahoo.com>
2015-04-24 17:25:01 -0400

    more food tests: get food by id update food - add measures, update name

--- server/app/routes/foodApi.js
+++ server/app/routes/foodApi.js
@@ -1,20 +1,44 @@
 var Food = require('../../app/models/food.js');
 
 module.exports = function(app) {
-  app.post('/food', createFood);
+  app.post('/food', createUpdateFood);
   app.get('/food/:id',getFoodById);
 
-  function createFood(req, res) {
-    var food = new Food(req.body);
+  function createUpdateFood(req, res) {
+    var food = req.body;
+    //console.log(food);
+    if(food._id){
+      console.log('food has id : ', food);
+      Food.findOne(food._id,function(err,f){
+        if(err){
+          throw err;
+        }
 
-    food.save(function(err){
-      if(err){
-        console.log("****   Error saving food *** : " + err)
-      }else{
-        res.send({newFood:food});
-      }
+        //console.log('******* f before : ',f);
+        f = food;
+        //console.log('******* f after ', f);
 
-    });
+        Food(f).update({upsert:true},function(err,fd){
+          if(err){
+            throw err;
+          }
+
+        });
+        res.send({food:f});
+      })
+    }else{
+      var newFood = new Food(food);
+      console.log('Creating new food : ', newFood);
+      newFood.save(function(err){
+        if(err){
+          console.log("****   Error saving food *** : " + err)
+        }else{
+          res.send({food:newFood});
+        }
+
+      });
+    }
+
   }
 
   function getFoodById(req,res){
--- server/test/food-spec.js
+++ server/test/food-spec.js
@@ -28,9 +28,12 @@ describe('create update get and delete food', function(){
             throw err;
           }
 
-          resp.body.should.have.property('newFood');
-          resp.body.newFood.should.have.property('_id');
-          newFoodId = resp.body.newFood._id;
+          resp.body.should.have.property('food');
+          resp.body.food.should.have.property('_id');
+          resp.body.food.should.have.property('foodMeasures');
+          resp.body.food.foodMeasures.length.should.equal(2);
+          newFoodId = resp.body.food._id;
+          food=resp.body.food;
           done();
         })
     });
@@ -50,6 +53,67 @@ describe('create update get and delete food', function(){
           resp.body.food._id.should.equal(newFoodId);
           done();
         })
-    })
+    });
+
+    it('should update a food - add a new measure', function(done){
+      var measure = new Measure({measureId:3,measureName:'test measure3'});
+      food.foodMeasures.push(measure);
+      console.log(food);
+
+      agent
+        .post('/food')
+        .send(food)
+        .expect(200)
+        .end(function(err,resp){
+          if(err){
+            throw(err);
+          }
+          resp.body.should.have.property('food');
+          resp.body.food._id.should.equal(newFoodId);
+          resp.body.food.foodMeasures.length.should.equal(3);
+          done();
+        })
+    });
+
+    it('should update a food - remove a measure', function(done){
+
+      food.foodMeasures.pop();
+      console.log(food);
+
+      agent
+        .post('/food')
+        .send(food)
+        .expect(200)
+        .end(function(err,resp){
+          if(err){
+            throw(err);
+          }
+          resp.body.should.have.property('food');
+          resp.body.food._id.should.equal(newFoodId);
+          resp.body.food.foodMeasures.length.should.equal(2);
+          done();
+        })
+    });
+
+    it('should update the name of the food',function(done){
+      //var measure = new Measure({measureId:3,measureName:'test measure3'});
+      var newName = 'Updated food name';
+      food.foodName = newName;
+      console.log(food);
+
+      agent
+        .post('/food')
+        .send(food)
+        .expect(200)
+        .end(function(err,resp){
+          if(err){
+            throw(err);
+          }
+          resp.body.should.have.property('food');
+          resp.body.food._id.should.equal(newFoodId);
+          resp.body.food.foodName.should.equal(newName);
+          done();
+        })
+    });
   })
 });
